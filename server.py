8175982af8c8019cc77352b0c4af2d69e48a16a2
Jayyyu1w <jayyu1w@gmail.com>
2024-08-31 20:40:09 +0800

    refactor: rewrite line-bot-sdk to v3 version

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 import os
 import datetime
 from llm_extract import get_LLM_response
@@ -14,7 +28,7 @@ with open("lineapi.txt", "r", encoding="utf-8") as f:
     WEBHOOK = f.readline().strip()
 
 # 設置Line Messaging API
-line_bot_api = LineBotApi(LINEAPI)
+configuration = Configuration(access_token=LINEAPI)
 handler = WebhookHandler(WEBHOOK)
 
 
@@ -22,16 +36,20 @@ handler = WebhookHandler(WEBHOOK)
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    
+    app.logger.info("Request body: " + body)
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
     reply_message = get_LLM_response(event.message.text)
     print(reply_message)
     print(type(reply_message["purpose"]))
@@ -39,10 +57,18 @@ def handle_message(event):
     try:
         calendar.add_event(reply_message)
         reply = "日曆事件新增成功"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply)])
+        )
     except:
         reply = "日曆事件新增失敗"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply)])
+        )
 
 
 if __name__ == "__main__":
